6f0d4489f9cb0b3d226a88eb2aa51eef99c4d8d6
Ghostwire <gitlab_admin_8d6963@example.com>
2026-02-03 18:22:57 +0000

    refactor: Abstract base module and slim spider host config

--- hosts/spider/configuration.nix
+++ hosts/spider/configuration.nix
@@ -1,51 +1,32 @@
-{ config, lib, pkgs, ... }:
-
 {
   imports = [
     ./hardware-configuration.nix
+    ../../modules/nixos/base.nix
   ];
+
+  # keep spider-specific stuff here for now
+  networking.hostName = "spider";
+  networking.networkmanager.enable = true;
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  networking.hostName = "spider";
-  networking.networkmanager.enable = true;
-  time.timeZone = "Europe/London";
 
   services.xserver = {
     enable = true;
     autoRepeatDelay = 200;
     autoRepeatInterval = 35;
     windowManager.qtile.enable = true;
-  }; 
-  services.displayManager.ly.enable = true;
+  };
 
-  # Enable touchpad support (enabled default in most desktopManager).
+  services.displayManager.ly.enable = true;
   services.libinput.enable = true;
 
   users.users.eddie = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
-      tree
-    ];
+    extraGroups = [ "wheel" ];
   };
 
   programs.firefox.enable = true;
 
-  environment.systemPackages = with pkgs; [
-    vim
-    wget
-    git
-    alacritty
-  ];
-
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-  ];
-
-  services.openssh.enable = true;  
-
-  nix.settings.experimental-features = ["nix-command" "flakes"];
   system.stateVersion = "25.11";
 }
-
---
+++ modules/nixos/base.nix
@@ -0,0 +1,21 @@
+{ config, lib, pkgs, ... }:
+
+{
+  time.timeZone = "Europe/London";
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    git
+    alacritty
+    tree
+  ];
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+  ];
+
+  services.openssh.enable = true;
+}
