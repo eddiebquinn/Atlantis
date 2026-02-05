abcba6e5d5e97e512d8c3e8e4cd82435b6e5c58a
Ghostwire <gitlab_admin_8d6963@example.com>
2026-02-05 01:01:01 +0000

    feat: Added config for zsh

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -1,16 +1,44 @@
 { config, pkgs, ... }:
 
 {
+
+  # Tools that make any shell nicer
+  home.packages = with pkgs; [
+    fastfetch
+  ];
+
   programs.bash = {
     enable = true;
     shellAliases = {
       hm-test = "echo HM Running";
     };
-
-  initExtra = ''
-    if [[ $- == *i* ]] && [[ -z "$SSH_CONNECTION" ]]; then
-      ${pkgs.fastfetch}/bin/fastfetch
-    fi
-  '';
   };
+
+
+  # Enable zsh (this becomes your interactive shell)
+  programs.zsh = {
+    enable = true;
+
+    enableCompletion = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+
+    history = {
+      size = 10000;
+      save = 10000;
+    };
+
+    shellAliases = {
+      rebuild = "sudo nixos-rebuild switch --flake /etc/nixos";
+      ll = "ls -lah";
+      gs = "git status";
+    };
+
+    initExtra = ''
+      if [[ $- == *i* ]] && [[ -z "$SSH_CONNECTION" ]]; then
+        ${pkgs.fastfetch}/bin/fastfetch
+      fi
+    '';
+  };
+  
 }
