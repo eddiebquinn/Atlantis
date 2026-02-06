b3d2c7cd2792511b5732198f541f88deb591cf42
Ghostwire <gitlab_admin_8d6963@example.com>
2026-02-06 01:26:16 +0000

    feat: Hardened SSH

--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -7,5 +7,10 @@
     isNormalUser = true;
     extraGroups = [ "wheel" /* etc */ ];
     shell = pkgs.zsh;
+    users.users.eddie.openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINt1KNkGPH+YPWl6qHX0PhbsevkEw8H1R86nOVV03k8Z (none)" # spider
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF+zfnhEIwiI2u03QnRjghuAYFQRbdzqT1pXHWD69eaG (none)" #blackhandO
+    ];
+
   };
 }
