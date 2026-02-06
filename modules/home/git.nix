d0bf5f53322343ecec98b5108c90e67bc345af48
Ghostwire <gitlab_admin_8d6963@example.com>
2026-02-06 02:16:16 +0000

    feat: Aadde further info to git config

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -4,6 +4,9 @@
   programs.git = {
     enable = true;
 
+    userName = "ghostwire";
+    userEmail = "eddie@b-quinn.com";
+
     signing = {
       key = "CA98D5946FA3A374BA7E2D8FB254FBF3F060B796";
       signByDefault = true;
