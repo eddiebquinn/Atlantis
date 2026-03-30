dccfe3b18d82a61fdb5c726c8fd56be824444388
ghostwire <eddie@b-quinn.com>
2026-03-30 09:01:22 +0100

    feat: Added steam

---
+++ modules/nixos/gaming.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+
+{
+
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  programs.steam.enable = true;
+  programs.steam.gamescopeSession.enable = true;
+  programs.gamemode.enable = true;
+
+}
